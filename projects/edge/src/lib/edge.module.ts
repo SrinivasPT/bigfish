8de40001391fb7af2fb22cf13c59d3dda631e4b5
Srinivas Peeta <srinivaspt@outlook.com>
2020-08-13 19:41:19 +0530

    Initia version

---
+++ projects/edge/src/lib/edge.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { EdgeComponent } from './edge.component';
+
+
+
+@NgModule({
+  declarations: [EdgeComponent],
+  imports: [
+  ],
+  exports: [EdgeComponent]
+})
+export class EdgeModule { }
